0b945c1a8e95fc0809d2048303c8ff77995a644a
sandeepvarghese7 <sandeepvarghese7@gmail.com>
2025-08-07 09:41:27 +0530

    added vowelsswitchcasedemo and forloopeven files

---
+++ Vowelswitchcasedemo.java
@@ -0,0 +1,31 @@
+package Coding;
+
+public class Vowelswitchcasedemo {
+
+	public static void main(String[] args) {
+	 char vowel='s';
+	 switch(vowel)
+	 {
+	 case 'a':
+	 case 'e':
+     case 'i':
+     case 'o':
+     case 'u':
+     case 'A':
+     case 'E':
+     case 'I':
+     case 'O':
+     case 'U':
+      System.out.print("its a vowel");
+	 
+      break;
+    
+      default:
+    	  System.out.print("not a vowel");
+	 }
+	 
+	 }
+
+	}
+
+
